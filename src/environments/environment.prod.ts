550f9d0ed734211758b4d3d9d0e262870f9e1871
marino <marino.menendez@gmail.com>
2026-07-10 09:48:45 +0200

    Eliminar env

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-
-  supabaseUrl: 'https://fcmtjycampqtvpacpngh.supabase.co',
-  supabaseKey: 'sb_publishable_N4109KlXQUivQCW4jxmUYw_UZwrBKvR'
-
-};
